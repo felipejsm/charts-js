44813b67769622fd3a7152268b8d37234da182f7
Felipe Moysés <felipe.moyses@techne.com.br>
2018-01-12 12:25:14 -0200

    Multi charts w/ ang1 DONE

--- chartjs/app3.js
+++ chartjs/app3.js
@@ -322,7 +322,7 @@ app.controller('myCtrl', function($scope) {
                         'unidadeEnsino' : unidade.unidadeEnsino,
                         'totalEvadidos' : parseInt(unidade.object[index].totalEvadidos),
                         'totalAlunos' :  parseInt(unidade.object[index].totalAlunos),
-                        'ano' : unidade.object[index].ano,
+                        'ano' : parseInt(unidade.object[index].ano),
                         'periodo' : unidade.object[index].periodo,
                         "unidadeFisica": unidade.object[index].unidadeFisica,
                         "nomeUnidadeFisica": unidade.object[index].nomeUnidadeFisica,
@@ -339,8 +339,92 @@ app.controller('myCtrl', function($scope) {
 
       montarHistorico();
     function montarHistorico() {
-        let agrupamento = groupByUnidadeEnsino(res);
+      let agrupamento = groupByUnidadeEnsino(res);
+      montarObjeto(agrupamento)
+      vm.labels             = [];
+      vm.data               = [];
+      vm.dataDoughnut       = [];
+      vm.dadosEvadidos      = [];
+      vm.dadosMatriculados  = [];
+      vm.options            = { legend: { display: true } };
+      vm.porcentagem        = [];
+      vm.nomeUnidadeEnsino  = [];
+      vm.colors = [{
+          backgroundColor: '#DB2C1F',
+          pointBackgroundColor: '#FF6384'
+      }, {
+          backgroundColor: '#3E95CD',
+          pointBackgroundColor: '#36A2EB'
+      }];
+      vm.colors2 = ['#DB2C1F','#3E95CD'];
+      vm.series = ['Evadidos', 'Matriculados'];
+
+     
+      let qtdGraficos = agrupamento.length;
+      let graficosInterno = [];
+      let graficos =  _(vm.historico)
+                    .groupBy(eva => eva.unidadeEnsino)
+                    .map((objs, key) => ({
+                        'unidadeEnsino': key,
+                        'nomeUnidadeEnsino' : objs[0].nomeUnidadeEnsino,
+                        'object' : objs
+                    })).value();
+      for(let indice = 0; indice < qtdGraficos; indice++) {
+             graficosInterno.push(_(graficos[indice].object)
+                     .groupBy(eva => eva.ano)
+                     .map((objs, key) => ({
+                         'anoPeriodo': objs[0].anoPeriodo,
+                         'totalMatriculados': _.sumBy(objs, 'totalAlunos'),
+                         'totalEvadidos'    : _.sumBy(objs, 'totalEvadidos'),
+                         'object'           : objs
+                     })).value());
+      }
+      for(let index = 0, len = qtdGraficos; index < len; index++) {
+        vm.dadosEvadidos = [];
+        vm.dadosMatriculados = [];
+        vm.labelsAux = [];
+        vm.nomeUnidadeEnsino.push(graficos[index].nomeUnidadeEnsino);
+        for(let indexIn = 0, lenIn = graficosInterno[index].length; indexIn < lenIn; indexIn++) {
+          vm.dadosEvadidos.push(parseInt(graficosInterno[index][indexIn].totalEvadidos));
+          vm.dadosMatriculados.push(parseInt(graficosInterno[index][indexIn].totalMatriculados));
+          vm.labelsAux.push(graficosInterno[index][indexIn].anoPeriodo);
+        }
+        vm.labels.push(vm.labelsAux);
+        vm.data.push([vm.dadosEvadidos, vm.dadosMatriculados]);
+        totalEvadidos = vm.dadosEvadidos.reduce(function(valorAnterior, valorAtual) {
+          return valorAnterior + valorAtual;
+        });
+        totalMatriculados = vm.dadosMatriculados.reduce(function(valorAnterior, valorAtual) {
+            return valorAnterior + valorAtual;
+        });
+        vm.dataDoughnut.push([totalEvadidos, totalMatriculados]);
+        vm.porcentagem.push(Math.floor((totalEvadidos / totalMatriculados) * 100));
+      }
+
+
+
+
+/* 
+
+      vm.labels.push(['2006', '2007', '2008', '2009', '2010', '2011', '2012']);
+      vm.labels.push(['2010', '2017', '2018', '2019', '2020', '2011', '2022']);
+     // $scope.series = ['Series A', 'Series B'];
+
+      vm.data.push([
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ]);
+      vm.data.push([
+        [50, 49, 60, 71, 76, 66, 20],
+        [38, 48, 20, 9, 86, 28, 45]
+      ]); */
+        // agrupamentos default
+       /*  let agrupamento = groupByUnidadeEnsino(res);
         montarObjeto(agrupamento)
+
+        vm.barData      = [];
+        vm.doughnutData = [];
+
         let dataSeriesEvadidos = [];
         let dataSeriesMatriculados = [];
         vm.labels = [];
@@ -350,6 +434,8 @@ app.controller('myCtrl', function($scope) {
         vm.data2 = [];
         let totalEvadidos;
         let totalMatriculados;
+        vm.labelsAux = [];
+        // agrupa por unidade ensino
         let graficos =  _(vm.historico)
                  .groupBy(eva => eva.unidadeEnsino)
                  .map((objs, key) => ({
@@ -359,6 +445,7 @@ app.controller('myCtrl', function($scope) {
          let qtdGraficos = graficos.length;
          let graficosInterno = [];
          for(let indice = 0; indice < qtdGraficos; indice++) {
+            // agrupa por ano periodo
              graficosInterno.push(_(graficos[indice].object)
                      .groupBy(eva => eva.anoPeriodo)
                      .map((objs, key) => ({
@@ -368,18 +455,20 @@ app.controller('myCtrl', function($scope) {
                          'object'           : objs
                      })).value());
          }
-         vm.colors2 = ['#DB2C1F','#3E95CD'];
          //vm.colors = ['#DB2C1F','#3E95CD'] ['#e86a6a', '#828ff2'];
          for(let index = 0, len = qtdGraficos; index < len; index++) {
              vm.data.push([]);
              dataSeriesEvadidos      = [];
              dataSeriesMatriculados  = [];
+             vm.labelsAux = [];
+             vm.labels.push([]);
+             
              for(let indexIn = 0, lenIn = graficosInterno[index].length; indexIn < lenIn; indexIn++) {
-                 vm.labels.push([]);
-                 vm.labels[indexIn].push(graficosInterno[index][indexIn].anoPeriodo);
+                 vm.labelsAux.push(graficosInterno[index][indexIn].anoPeriodo);
                  dataSeriesEvadidos.push(graficosInterno[index][indexIn].totalEvadidos);
                  dataSeriesMatriculados.push(graficosInterno[index][indexIn].totalMatriculados);
              }
+             vm.labels[index].push(vm.labelsAux);
              vm.data[index].push(dataSeriesEvadidos);
              vm.data[index].push(dataSeriesMatriculados);
              totalEvadidos = dataSeriesEvadidos.reduce(function(valorAnterior, valorAtual) {
@@ -398,6 +487,7 @@ app.controller('myCtrl', function($scope) {
              backgroundColor: '#3E95CD',
              pointBackgroundColor: '#36A2EB'
          }];
-         vm.series = ['Evadidos', 'Matriculados'];
+         vm.colors2 = ['#DB2C1F','#3E95CD'];
+         vm.series = ['Evadidos', 'Matriculados']; */
      }
 })
